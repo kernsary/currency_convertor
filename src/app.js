ae41176053b98c63eb55e4842204e125fa5a3ceb
Charles Kennedy <charlesscottkennedy@googlemail.com>
2019-10-22 20:35:05 +0100

    started for loop

--- src/app.js
+++ src/app.js
@@ -4,18 +4,39 @@ document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
     data: {
-      rates: []
+      currencies: [],
+      euroInputAmount: null,
+      currency: null
+      // name: null,
+      // rate: null,
+      // index: null
     },
 
     mounted() {
       this.fetchRates();
+      console.log(this.currencies[1]);
+    },
+
+    computed: {
+
+      // name: function() {
+      //   return this.currencies[this.index].name
+      // },
+      //
+      // rate: function() {
+      //   return this.currencies[this.index].value
+      // },
+
+      targetCurrencyValue: function() {
+        return this.euroInputAmount * this.rate
+      }
     },
 
     methods: {
       fetchRates: function() {
         fetch("https://api.exchangeratesapi.io/latest")
         .then(response => response.json())
-        .then(data => this.rates = data.rates)
+        .then(data => this.currencies = data.rates)
       }
     }
   })
